421a74a45106542f0b44b8f2d34fd20bb5dc5d76
Pablo Monardo <pablomonardo@gmail.com>
2025-02-08 22:01:07 -0300

    archivos utiles

---
+++ src/components/icons/index.ts
@@ -0,0 +1,23 @@
+// Iconos de tecnologías
+export { default as AstroIcon } from "./AstroIcon.astro";
+export { default as C } from "./CIcon.astro";
+export { default as CSS } from "./CSSIcon.astro";
+export { default as GitHub } from "./GitHub.astro";
+export { default as GitLab } from "./GitLabIcon.astro";
+export { default as HTML } from "./HTMLIcon.astro";
+export { default as JavaScript } from "./JavaScriptIcon.astro";
+export { default as MD } from "./MDIcon.astro";
+export { default as MySQL } from "./MySQLIcon.astro";
+export { default as Nextjs } from "./NextjsIcon.astro";
+export { default as Python } from "./PythonIcon.astro";
+export { default as React } from "./ReactIcon.astro";
+export { default as SASS } from "./SASSIcon.astro";
+export { default as SQLIcon } from "./SqlIcon.astro";
+export { default as SQLServer } from "./SQLServerIcon.astro";
+export { default as TailwindIcon } from "./TailwindIcon.astro";
+export { default as TypeScript } from "./TypeScript.astro";
+
+// Utilidades
+export { default as ExperienceIcon } from "./Experience.astro";
+export { default as Linkedin } from "./LinkedIn.astro";
+export { default as Mail } from "./Mail.astro";
